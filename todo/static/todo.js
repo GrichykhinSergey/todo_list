2f1afd710237beba59fbc129515b34c18650c76b
GrichykhinSergey <90179867+GrichykhinSergey@users.noreply.github.com>
2021-09-22 18:29:40 +0300

    Add files via upload

---
+++ todo/static/todo.js
@@ -0,0 +1,232 @@
+const allOptions = document.querySelector('#all');
+const inProgress = document.querySelector('#progress');
+const completed = document.querySelector('#completed');
+const input = document.querySelector('#input');
+const ul = document.querySelector('#toDos');
+const addBtn = document.querySelector('#add');
+const saveBtn = document.querySelector('#save');
+const cancelBtn = document.querySelector('#cancel');
+let ids = [];
+
+const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
+
+const deleteHandler = () => {
+    const deleteBtn = document.querySelectorAll('.deleteBtn');
+    deleteBtn.forEach((el) => el.onclick = () => fetch('/delete_item/', {
+        method: 'POST',
+        headers: {'X-CSRFToken': csrftoken},
+        body: JSON.stringify({'id': el.parentElement.id.slice(2), 'to_delete': true})
+    }).then((response) => response.text())
+        .then((item_id) => ids.splice(+item_id, 1))
+        .then(() => el.parentElement.remove()))
+}
+
+const editHandler = () => {
+    const editBtn = document.querySelectorAll('.editBtn');
+    editBtn.forEach((el) => {
+        el.onclick = () => {
+            const editingEl = document.querySelector('.editing');
+            if (editingEl) editingEl.classList.remove('editing');
+
+            input.value = el.parentElement.lastChild.textContent;
+            input.setAttribute('class', el.parentElement.id);
+            addBtn.setAttribute('disabled', 'disabled');
+            addBtn.classList.add('disabled');
+            el.parentElement.classList.add('editing');
+            saveBtn.removeAttribute('hidden');
+            cancelBtn.removeAttribute('hidden');
+        }})
+}
+
+saveBtn.onclick = () => {
+        const liEl = document.querySelector(`#${input.className}`);
+        console.log(input.className);
+        if (checkStringLength()) {
+            fetch('/edit_item/', {
+                method: 'PATCH',
+                headers: {'X-CSRFToken': csrftoken},
+                body: JSON.stringify({'id': liEl.id.slice(2), 'content': input.value, 'item_to_update': true})
+            })
+                .then((response) => response.json())
+                .then((result) =>  liEl.lastChild.textContent = result.data)  //target.lastChild = result.data)
+                .then(() => liEl.classList.remove('editing'))
+                .then(() => input.value = '')
+                .then(() => addBtn.removeAttribute('disabled'))
+                .then(() => addBtn.classList.remove('disabled'))
+                .then(() => saveBtn.setAttribute('hidden', ''))
+                .then(() => cancelBtn.setAttribute('hidden', ''))
+        }
+    }
+
+cancelBtn.onclick = () => {
+    const liEl = document.querySelector(`#${input.className}`);
+    input.value = '';
+    addBtn.removeAttribute('disabled');
+    addBtn.classList.remove('disabled');
+    saveBtn.setAttribute('hidden', '');
+    cancelBtn.setAttribute('hidden', '');
+    liEl.classList.remove('editing');
+}
+
+const completedTasksHandler = () => {
+    const editBtn = document.querySelectorAll('.completedBtn');
+    editBtn.forEach((el) => el.onclick = () => {
+        el.parentElement.classList.toggle('checked');
+
+        if (completed.classList.contains('selected')) {
+            filterElements(true);
+        } else if (inProgress.classList.contains('selected')) {
+            filterElements(false);
+        }
+        fetch('/completed_item/', {
+            method: 'PATCH',
+            headers: {'X-CSRFToken': csrftoken},
+            body: JSON.stringify({'id': el.parentElement.id.slice(2), 'completed': el.parentElement.classList.contains('checked'), 'to_change_state': true})
+        })
+            .then((response) => response.json())
+            .then((result) => console.log(result['result']))
+  })
+}
+
+const checkStringLength = () => {
+  if (input.value.length > 100) {
+    input.value = '';
+    input.setAttribute('placeholder', 'string length must not be more than 100 symbols');
+    return false;
+  } else if (input.value.trim().length === 0) return false;
+  
+  input.setAttribute('placeholder', '');
+  return true;
+}
+
+const createElement = (content) => {
+  if (!ids.includes(content.id) && content.data.trim().length > 0) {
+      const li = document.createElement('li');
+      li.setAttribute('id', `id${content.id}`);
+      li.setAttribute('tabindex', '0');
+      const text = document.createTextNode(content.data);
+      li.insertAdjacentHTML('beforeend', `<button class="deleteBtn" contenteditable="false">❌</button>
+      <button class="completedBtn" id="taskCompleted" contenteditable="false" >✅</button>
+      <button class="editBtn" contenteditable="false">Edit</button>`);
+      li.append(text);
+
+      if (content.completed) {
+          li.className = 'checked';
+      }
+
+      if (completed.classList.contains('selected')) {
+          li.style.display = 'none';
+      } else {
+          li.style.display = 'block';
+      }
+
+      ids.push(content.id);
+      ul.append(li);
+      input.value = '';
+      console.log(ids);
+  }
+  deleteHandler();
+  completedTasksHandler();
+  editHandler();
+}
+
+const updateElements = () => {
+  const li = document.querySelectorAll('li');
+  li.forEach((el) => el.style.display = 'block');
+}
+
+const filterElements = (isCompleted) => {
+  const li = document.querySelectorAll('li');
+  
+  li.forEach((el) => {
+    if (isCompleted) {
+      el.style.display = el.className === 'checked' ? 'block' : 'none';
+    } else {
+      el.style.display = el.className !== 'checked' ? 'block' : 'none';
+    }
+  });
+}
+
+fetch('/all_items/')
+    .then((response) => response.json())
+    .then((result) => result.forEach(item => createElement(item)))
+
+add.onclick = () => {
+    if (checkStringLength()) {
+        fetch('/add/', {
+            method: 'POST',
+            headers: {'X-CSRFToken': csrftoken},
+            body: JSON.stringify({'content': input.value, 'to_add': true})
+        })
+            .then((response) => response.json())
+            .then((result) => createElement(result))
+
+    }
+}
+
+document.addEventListener('keydown', (event) => {
+        if (event.code === 'Enter') {
+            if (checkStringLength()) {
+                fetch('/add/', {
+                    method: 'POST',
+                    headers: {'X-CSRFToken': csrftoken},
+                    body: JSON.stringify({'content': input.value, 'to_add': true})})
+                    .then((response) => response.json())
+                    .then((result) => createElement(result))
+                    }
+        }
+    }
+)
+
+all.onclick = () => {
+  updateElements();
+  allOptions.classList.add('selected');
+  completed.classList.remove('selected');
+  inProgress.classList.remove('selected');
+}
+
+progress.onclick = () => {
+  filterElements(false);
+  inProgress.classList.add('selected');
+  completed.classList.remove('selected');
+  allOptions.classList.remove('selected');
+
+}
+
+completed.onclick = () => {
+  filterElements(true);
+  completed.classList.add('selected');
+  allOptions.classList.remove('selected');
+  inProgress.classList.remove('selected');
+}
+
+clear.onclick = () => {
+    fetch('/clear_completed_tasks/', {
+            method: 'POST',
+            headers: {'X-CSRFToken': csrftoken},
+            body: JSON.stringify({'to_delete_all': true})
+    })
+        .then(() => document.querySelectorAll('.deleteBtn')
+            .forEach((el) => {
+                if (el.parentElement.classList.contains('checked')) {
+                    el.parentElement.remove()
+                }
+            }
+            ))
+}
