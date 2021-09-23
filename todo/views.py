a9683ea75bd91fdbb152d867a840231be15f45c8
GrichykhinSergey <90179867+GrichykhinSergey@users.noreply.github.com>
2021-09-23 09:12:11 +0300

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -15,13 +15,13 @@ class AllItemsView(View):
     def patch(self, request):
         data = json.loads(request.body)
 
-        if data.get('item_to_update'):
+        if request.path == '/edit_item/':
             item_to_update = Task.objects.get(id=int(data['id']))
             item_to_update.data = data['content']
             item_to_update.save()
             updated_item = list(Task.objects.filter(id=data['id']).values())
             return JsonResponse(updated_item[-1], safe=False)
-        if data.get('to_change_state'):
+        if request.path == '/completed_item/':
             item_to_change_state = Task.objects.get(id=data['id'])
             item_to_change_state.completed = data['completed']
             item_to_change_state.save()
@@ -30,20 +30,20 @@ class AllItemsView(View):
     def post(self, request):
         data = json.loads(request.body)
 
-        if data.get('to_add'):
+        if request.path == '/add/':
             new_item = Task(data=data['content'])
             new_item.save()
             item_data = list(Task.objects.order_by('id').filter(data=data['content']).values())
             return JsonResponse(item_data[-1], safe=False)
-        elif data.get('to_delete'):
+        elif request.path == '/delete_item/':
             item_to_delete = Task.objects.get(id=data['id'])
             item_to_delete.delete()
             return HttpResponse(data['id'])
-        elif data.get('to_delete_all'):
+        elif request.path == '/clear_completed_tasks/':
             completed_items = Task.objects.filter(completed=True)
             completed_items.delete()
             return JsonResponse({'result': 'success'})
 
     def get(self, request):
-        all_items = list(Task.objects.values())
+        all_items = list(Task.objects.order_by('id').values())
         return JsonResponse(all_items, safe=False)
